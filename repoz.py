04de66f55d29d90a71e37a1ad285ad3d8a7c5a90
Dima7776767 <138670505+Dima7776767@users.noreply.github.com>
2023-07-10 18:31:31 +0500

    Update repoz.py

--- repoz.py
+++ repoz.py
@@ -1,6 +1,20 @@
-stro = str(input('Введите палиндром: '))
-a = stro[::-1]
-if stro == a:
-    print('Это палиндром!')
-else:
-    print('Это не палиндрои!')
+
+'''
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>дз</title>
+</head>
+<body>
+    
+    <h1>Текст.1</h1>
+    <h2>Текст.2</h2>
+    <img src="https://www.thesun.co.uk/wp-content/uploads/2018/02/nintchdbpict0003798039863.jpg" alt="">
+
+</body>
+</html>
+
+file:///C:/Users/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C/Desktop/page1.html
+'''
