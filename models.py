f77c2e78296156f0023f869f5148c57707907ada
Dima7776767 <138670505+Dima7776767@users.noreply.github.com>
2023-08-07 13:15:24 +0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=128, verbose_name='Заголовок')
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если уместен торг')
