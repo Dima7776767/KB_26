c6bd2758b15fd9793aa5a75b92a99704eaadf4ee
Dima7776767 <138670505+Dima7776767@users.noreply.github.com>
2023-08-07 13:19:43 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,16 @@
-from django.db import models
+# from django.db import models
 
 
-class Advertisement(models.Model):
-    title = models.CharField(max_length=128, verbose_name='Заголовок')
-    description = models.TextField('Описание')
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    auction = models.BooleanField('Торг', help_text='Отметьте, если уместен торг')
+# class Advertisement(models.Model):
+#     title = models.CharField(max_length=128, verbose_name='Заголовок')
+#     description = models.TextField('Описание')
+#     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+#     create_at = models.DateTimeField(auto_now_add=True)
+#     update_at = models.DateTimeField(auto_now=True)
+#     auction = models.BooleanField('Торг', help_text='Отметьте, если уместен торг')
+
+#     def __str__(self):
+#         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
+#     class Meta:
+#         db_table = 'advertisements'
