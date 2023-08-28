47a6f8faa31429e905659ab397d334d6986a1734
Dima7776767 <138670505+Dima7776767@users.noreply.github.com>
2023-08-28 16:35:24 +0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class AppLesson4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_lesson_4'
+# class AppLesson4Config(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'app_lesson_4'
+#     verbose_name = 'Объявления'
